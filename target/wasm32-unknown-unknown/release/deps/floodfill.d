73c4872fc1bafaadda199766728d82bf5fa8b3b2
LEE JONGIN <leejongin213@gmail.com>
2025-04-07 19:34:49 +0900

    test

--- target/wasm32-unknown-unknown/release/deps/floodfill.d
+++ target/wasm32-unknown-unknown/release/deps/floodfill.d
@@ -1,5 +1,5 @@
-D:\Project\Paintive\floodfill\target\wasm32-unknown-unknown\release\deps\floodfill.wasm: src\lib.rs
+D:\Project\Paintive\floodfill\Fill\target\wasm32-unknown-unknown\release\deps\floodfill.wasm: src\lib.rs
 
-D:\Project\Paintive\floodfill\target\wasm32-unknown-unknown\release\deps\floodfill.d: src\lib.rs
+D:\Project\Paintive\floodfill\Fill\target\wasm32-unknown-unknown\release\deps\floodfill.d: src\lib.rs
 
 src\lib.rs:
--- target/wasm32-unknown-unknown/release/floodfill.d
+++ target/wasm32-unknown-unknown/release/floodfill.d
@@ -1,1 +1,1 @@
-D:\Project\Paintive\floodfill\target\wasm32-unknown-unknown\release\floodfill.wasm: D:\Project\Paintive\floodfill\src\lib.rs
+D:\Project\Paintive\floodfill\Fill\target\wasm32-unknown-unknown\release\floodfill.wasm: D:\Project\Paintive\floodfill\Fill\src\lib.rs
